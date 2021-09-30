0de423be32127878fba923d942118145c3d608c6
ACHRAF NAJMI <najmi.achraf@gmail.com>
2021-10-01 00:51:05 +0100

    Update Infographics 2: 	modified:   README.md 	modified:   docs/media/infographics2_en.png 	modified:   docs/media/infographics2_fr.png 	deleted:    infographics2/en/compile_infographics2_en.bat 	modified:   infographics2/en/main.pdf 	modified:   infographics2/en/main.tex 	deleted:    infographics2/fr/compile_infographics2_fr.bat 	modified:   infographics2/fr/main.pdf 	modified:   infographics2/fr/main.tex

--- infographics2/en/compile_infographics2_en.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-pdflatex main.tex
-
-del *.aux *.log *.out main.synctex.gz
-
-magick -density 192 "main.pdf" -resize 826 -depth 8 -quality 96 infographics2_en.png
-move /Y ./"infographics2_en.png" ../../docs/media/
