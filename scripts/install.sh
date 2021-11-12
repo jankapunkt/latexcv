a10187623413bad726e4c865a6e624704d9d2b29
Jan Küster <jkuester@uni-bremen.de>
2021-11-12 15:22:53 +0100

    update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,33 +13,33 @@ print "Unpack texlive"
 tar -xvzf install-tl-unx.tar.gz
 rm install-tl-unx.tar.gz
 
-echo "Prepare texlive installation in $(pwd):"
+print "Prepare texlive installation in $PROJECT_DIR:"
 cd install-tl-*/
 chmod +x install-tl
 
-echo "Install texlive from profile: texlive.profile"
+print "Install texlive from profile: texlive.profile"
 sudo ./install-tl --profile=$PROJECT_DIR/texlive.profile
 
-echo "Expand PATH"
+print "Expand PATH"
 YEAR=$(date +'%Y')
 PATH=/usr/local/texlive/${YEAR}/bin/x86_64-linux:$PATH
 
-echo "Check for latex version:"
+print "Check for latex version:"
 pdflatex -v
 
-echo "Install additional packages:"
-tlmgr install xifthen
-tlmgr install ifmtarg
-tlmgr install gillius
-tlmgr install xkeyval
-tlmgr install fontaxes
-tlmgr install moresize
-tlmgr install fontawesome
-tlmgr install multirow
-tlmgr install wrapfig
-tlmgr install float
-tlmgr install pgf
-tlmgr install transparent
+print "Install additional packages:"
+sudo tlmgr install xifthen
+sudo tlmgr install ifmtarg
+sudo tlmgr install gillius
+sudo tlmgr install xkeyval
+sudo tlmgr install fontaxes
+sudo tlmgr install moresize
+sudo tlmgr install fontawesome
+sudo tlmgr install multirow
+sudo tlmgr install wrapfig
+sudo tlmgr install float
+sudo tlmgr install pgf
+sudo tlmgr install transparent
 
 cd ${PROJECT_DIR}
 
